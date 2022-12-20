1234021148700768ff7655833364ad5815b1f82b
Rick Benavidez <rick.benavidez@gmail.com>
2022-12-20 19:07:48 +0000

    More specific check for bad href element when de-linking content.

--- lib/html/pipeline/strong_links/strong_links_filter.rb
+++ lib/html/pipeline/strong_links/strong_links_filter.rb
@@ -9,7 +9,7 @@ module HTML
           element["rel"] = "noopener noreferrer"
           element["target"] = "_blank"
           element["data-turbo"] = "false"
-          unless element["href"].to_s.start_with?("http")
+          if element["href"].to_s.match('script')
             # We have to create a new node from scratch in order to do a
             # replacement which will effectively strip the anchor from the doc.
             new_node = Nokogiri::XML::Text.new(element.inner_html, doc)
